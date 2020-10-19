44fa88785325078785d9ea462f2a6d2d7cd61755
werdien jihed <informatique.jihed@gmail.com>
2020-10-19 22:29:36 +0200

    first commit

---
+++ Contracts/ILeaveHistoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Leave_management.Data;
+
+namespace Leave_management.Contracts
+{
+	public interface ILeaveHistoryRepository : IRepositoryBase<LeaveHistory>
+    {
+    }
+}
---
+++ Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Leave_management.Data;
+using System.Collections.Generic;
+
+namespace Leave_management.Contracts
+{
+	public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeesByLeaveType(int id);
+    }
+}
